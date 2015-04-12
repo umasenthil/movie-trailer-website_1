7fb4dbf22232f48628933417782fb7b237ed3f77
umasenthil <uma_senthil@hotmail.com>
2015-04-12 08:57:24 -0700

    Committed by Udacity

---
+++ media.py
@@ -0,0 +1,11 @@
+class Movie():
+   def __init__(self,movie_title, movie_storyline, movie_poster_image_url, movie_trailer_url,release_date,actors):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = movie_poster_image_url
+        self.trailer_youtube_url = movie_trailer_url
+        self.release_date = release_date
+        self.actors = actors
+
+    
+    
